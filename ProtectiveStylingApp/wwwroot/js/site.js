c42e7e6e758d7e295d0e05eefe4e35aed22b73fe
Oluchi Ochulo <ochulo02@byu.edu>
2024-02-08 17:28:12 -0700

    Add project files.

---
+++ ProtectiveStylingApp/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $("#calculateButton").click(function () {
+        
+        var hours = parseFloat($("#hours").val());
+        var hourlyRate = parseFloat($("#hourlyRate").val());
+
+        // Validate the input
+        if (hours > 0) {
+            var total = hours * hourlyRate;
+            $("#total").val(total);
+        }
+        else {
+            alert("Please enter a valid number of hours");
+        }
+    });
+});
